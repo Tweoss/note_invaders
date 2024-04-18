0a64618cb2ce0c37850e500af7cbdf60a5b17faf
Francis Chua <fqchua@stanford.edu>
2024-04-18 09:23:28 -0700

    include js files

---
+++ index.js
@@ -0,0 +1,90 @@
+import { Note, round_frequency_to_note } from "./note.js";
+import { find_pitch } from "./pitch.js";
+export const TRACK_COUNT = 200 / 10;
+export const SVG_WINDOW = { x_min: -100, x_max: 100, y_min: -100, y_max: 100 };
+let audio_context, analyser, output_node, sample_buffer;
+const container = document.querySelector("#container");
+let game_active = false;
+let speed;
+let notes = new Map();
+function initialize() {
+    audio_context = new AudioContext();
+    analyser = audio_context.createAnalyser();
+    analyser.fftSize = 2048;
+    sample_buffer = new Float32Array(2048);
+    const compressor_node = new DynamicsCompressorNode(audio_context, {
+    // threshold: -35,
+    });
+    output_node = new GainNode(audio_context, { gain: 0.1 });
+    compressor_node.connect(output_node);
+    output_node.connect(audio_context.destination);
+    requestMicInput();
+    game_active = true;
+    speed = 0.2;
+    setInterval(handle_tick, 1000);
+    animate_boxes();
+}
+const button = document.querySelector("#start-button");
+button.onclick = () => {
+    button.remove();
+    initialize();
+};
+function requestMicInput() {
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream) => {
+        audio_context.createMediaStreamSource(stream).connect(analyser);
+    })
+        .catch((err) => {
+        console.error(err);
+        alert("Microphone input required to use website.");
+    });
+}
+function handle_tick() {
+    if (!game_active) {
+        return;
+    }
+    if (notes.size < 1) {
+        add_note();
+    }
+}
+function animate_boxes() {
+    if (!game_active) {
+        return;
+    }
+    speed *= 1.0003;
+    for (let [note, obj] of notes) {
+        obj.move(speed);
+        if (obj.y >= SVG_WINDOW.y_max) {
+            obj.cleanup();
+            notes.delete(note);
+            // game_over();
+        }
+    }
+    analyser.getFloatTimeDomainData(sample_buffer);
+    let pitch = find_pitch(sample_buffer, audio_context.sampleRate);
+    if (pitch) {
+        const note = round_frequency_to_note(pitch);
+        for (const [k, v] of notes) {
+            if (k % 12 == note % 12) {
+                v.success();
+                notes.delete(k);
+            }
+        }
+    }
+    requestAnimationFrame(animate_boxes);
+}
+function game_over() {
+    for (const [_, { audio_node }] of notes) {
+        audio_node.disconnect();
+    }
+    notes.clear();
+    game_active = false;
+}
+function add_note() {
+    const note = Math.round(Math.random() * 24 - 12);
+    if (notes.has(note)) {
+        return;
+    }
+    notes.set(note, new Note(audio_context, output_node, container, note));
+}
---
+++ note.js
@@ -0,0 +1,61 @@
+import { SVG_WINDOW, TRACK_COUNT } from "./index.js";
+export class Note {
+    constructor(context, output_node, svg_container, note) {
+        const oscillator = new OscillatorNode(context, {
+            type: "sine",
+            frequency: note_to_frequency(note),
+        });
+        const track = Math.floor(Math.random() * TRACK_COUNT);
+        this.y = -100;
+        this.context = context;
+        oscillator.start();
+        const gain = new GainNode(context, { gain: 0 });
+        oscillator.connect(gain);
+        gain.connect(output_node);
+        this.audio_node = gain;
+        this.svg = create_note_svg(track, this.y);
+        this.output_node = output_node;
+        console.log("constructing", note);
+        function create_note_svg(track, start_y) {
+            const svg = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+            const track_width = (SVG_WINDOW.x_max - SVG_WINDOW.x_min) / TRACK_COUNT;
+            svg.setAttribute("width", track_width.toString());
+            svg.setAttribute("height", "5");
+            svg.setAttribute("x", (SVG_WINDOW.x_min + track_width * track).toString());
+            svg.setAttribute("y", start_y.toString());
+            svg.setAttribute("fill", "red");
+            svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+            svg_container === null || svg_container === void 0 ? void 0 : svg_container.appendChild(svg);
+            return svg;
+        }
+    }
+    move(delta) {
+        this.y += delta;
+        this.svg.setAttribute("y", this.y.toString());
+        const transition = [SVG_WINDOW.y_min, SVG_WINDOW.y_min + 5];
+        const on = (v) => v < transition[0] ? 0 : Math.exp(-1 / (v - transition[0]));
+        const desired_volume = on(this.y) / (on(this.y) + on(transition[1] - this.y));
+        this.audio_node.gain.exponentialRampToValueAtTime(desired_volume, this.context.currentTime + 1 / 60);
+        this.audio_node;
+    }
+    success() {
+        this.svg.setAttribute("fill", "green");
+        setTimeout(() => this.svg.setAttribute("fill", "#0000"), 500);
+        setTimeout(() => this.cleanup(), 1000);
+    }
+    cleanup() {
+        const fadeout = 0.9;
+        this.audio_node.gain.exponentialRampToValueAtTime(0.0001, this.context.currentTime + fadeout);
+        setTimeout(() => {
+            this.audio_node.disconnect();
+        }, fadeout * 1000);
+        this.svg.remove();
+    }
+}
+// Take A = 440 as 0.
+function note_to_frequency(note) {
+    return 440 * Math.pow(2, (1 / 12) * note);
+}
+export function round_frequency_to_note(frequency) {
+    return Math.round(Math.log2(frequency / 440) * 12);
+}
